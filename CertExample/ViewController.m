78a6fba12ebb24c9183b437348387580fbeb4658
Denis Stanishevskiy <std.denis@gmail.com>
2015-05-26 12:56:54 +0300

    - initial commit with pods'ed stuff, due to example nature of the project

--- CertExample/ViewController.m
+++ CertExample/ViewController.m
@@ -7,21 +7,45 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UILabel *requestAddress;
 
 @end
 
 @implementation ViewController
+{
+    NSURL* _baseURL;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    _baseURL = [NSURL URLWithString:@"https://www.pcwebshop.co.uk/"];
+    _requestAddress.text = [NSString stringWithFormat:@"to %@", _baseURL];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)execute:(id)sender {
+    
+    AFHTTPSessionManager *client = [[AFHTTPSessionManager alloc] initWithBaseURL:_baseURL];
+    client.securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModePublicKey];
+    client.securityPolicy.allowInvalidCertificates = YES;
+    client.securityPolicy.validatesDomainName = NO;
+    
+    AFHTTPResponseSerializer *responseSerializer = [AFHTTPResponseSerializer serializer];
+    responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/html"];
+    client.responseSerializer = responseSerializer;
+    
+    [client GET:@"/" parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
+        _textView.text = [NSString stringWithFormat:@"Succeeded:\n\n%@", [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding]];
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+        _textView.text = [NSString stringWithFormat:@"Failed:\n\n%@", error];
+    }];
 }
 
 @end
