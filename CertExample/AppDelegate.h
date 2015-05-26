51cf166f88522f2f75bde3c201357e780a8a6c1c
Denis Stanishevskiy <std.denis@gmail.com>
2015-05-26 12:04:56 +0300

    Initial Commit

---
+++ CertExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CertExample
+//
+//  Created by dstd on 26/05/15.
+//  Copyright (c) 2015 stdlabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CertExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CertExample
+//
+//  Created by dstd on 26/05/15.
+//  Copyright (c) 2015 stdlabs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
